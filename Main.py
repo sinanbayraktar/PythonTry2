96d3ed95dc29f4bbc1339670b1e2453fdbfef97d
Sinan İbrahim Bayraktar <_sinan_@windowslive.com>
2017-06-16 03:19:27 +0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,17 @@
+import sys
+print ("this program adds two numbers you enter")
+print ("please enter the first number")
+x1 = sys.stdin.readline ()
+print ("please enter the second number")
+x2 = sys.stdin.readline ()
+result = int(x1) + int(x2)
+print (result)
+
+print ("second try")
+print ("please enter the first number")
+y1 = input ()
+print ("please enter the second number")
+y2 = input ()
+result2 = int(y1) + int(y2)
+print (result2)
+z = input()  # for stopping the close screen
